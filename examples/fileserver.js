6fd5a0bd8ac5e564a0378c5587481783d7291ba1
Reggie <cushingreggie@gmail.com>
2018-09-27 16:24:34 +0200

    pass auth as function

--- examples/fileserver.js
+++ examples/fileserver.js
@@ -7,21 +7,65 @@
  */
 "use strict";
 const config = require('./config.json')
+const jwt = require('jsonwebtoken')
 
 var jsDAV = require("./../lib/jsdav");
 //jsDAV.debugMode = true;
-jsDAV.superUsers = config.Users;
-jsDAV.enforceTokenAuth = config.enforceTokenAuth;
 
-Object.keys(jsDAV.superUsers).forEach((k) => {
-	const v = jsDAV.superUsers[k];
+Object.keys(config.Users).forEach((k) => {
+	const v = config.Users[k];
 	v.decodedPublicKey = Buffer.from(v.publicKey, 'base64').toString()
-	console.log(v)
 })
+
+
+var authWithToken = (function(config) {
+	var config = config;
+
+	return function(ref, req, res, next) {
+
+		function forbid(msg) {
+			if (msg) {
+				console.log(msg);
+			}
+			var headers = [];
+			res.writeHead(403, headers);
+			res.end();
+		}
+
+		if (!config.enforceTokenAuth) {
+			next(null, ref);
+			return;
+		}
+		var token = req.headers.authorization.replace('Bearer ','');
+		if (!token) {
+			forbid('No token');
+			return;
+		}
+		var preDecoded = jwt.decode(token);
+		if (!preDecoded) {
+			forbid('Error decoding token');
+			return;
+		}
+		var user = preDecoded.email;
+		var cert = config.Users[user].decodedPublicKey
+		if (!user || !config.Users || !config.Users[user] || !cert) {
+			forbid('No user');
+			return;
+		}
+		jwt.verify(token, cert, function(err, decoded) {
+			if (err) {
+				forbid(err);
+				return;
+			}
+			next(null, ref)
+		});
+	}
+})(config);
+
 
 var jsDAV_Locks_Backend_FS = require("./../lib/DAV/plugins/locks/fs");
 
 jsDAV.createServer({
     node: __dirname + "/../test/assets",
-    locksBackend: jsDAV_Locks_Backend_FS.new(__dirname + "/../test/assets")
-}, 8000);
+    locksBackend: jsDAV_Locks_Backend_FS.new(__dirname + "/../test/assets"),
+}, 8000, null, authWithToken);
